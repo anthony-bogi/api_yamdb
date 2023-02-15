9991caccf52ee9a4d0f0b542e387ac04004b9bcd
Антон Богданович <bogianthony@gmail.com>
2023-02-15 14:00:15 +0300

    put-secret_key-to-env-using-python_dotenv

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,10 +1,14 @@
+import os
 from pathlib import Path
+
+from dotenv import find_dotenv, load_dotenv
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
