4befe43e3c31dc39565153cfbb083531eb4a4360
Игорь Екимов <torgov88@gmail.com>
2023-02-17 16:51:43 +0300

    permission

---
+++ api_yamdb/api/v1/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework import permissions
+
+
+class IsAdminOrSuperuserPermission(permissions.BasePermission):
+    """Разрешение доступа только для Администратора и Суперпользователя."""
+    message = 'У Вас нет разрешения для дальнейшей работы!'
+
+    def has_permission(self, request, view):
+        if request.user.role == 'admin' or request.user.is_superuser:
+            return True
+        return False
+
+
+class IsModeratorPermission(permissions.BasePermission):
+    """Доступ разрешен только для Модератора."""
+    message = 'У Вас нет разрешения для дальнейшей работы!'
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.role == 'moderator':
+            return True
+        return False
--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -1,5 +1,8 @@
 from reviews.models import Category, Genre, Title
 from rest_framework import filters, viewsets
+from .permissions import (
+    IsAdminOrSuperuserPermission,
+)
 from .serializers import (
     CategorySerializer,
     GenreSerializer,
@@ -12,6 +15,7 @@ class TitleViewSet(viewsets.ModelViewSet):
     """Вьюсет для произведений."""
     queryset = Title.objects.all().order_by('name')
     serializer_class = TitleSerializer
+    permission_classes = IsAdminOrSuperuserPermission
 
     def get_serializer_class(self):
         if self.request.method in ('POST', 'PATCH', 'DELETE',):
@@ -23,6 +27,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
     """Вьюсет для категорий."""
     queryset = Category.objects.all().order_by('name')
     serializer_class = CategorySerializer
+    permission_classes = IsAdminOrSuperuserPermission
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
     lookup_field = 'slug'
@@ -32,6 +37,7 @@ class GenreViewSet(viewsets.ModelViewSet):
     """Вьюсет для жанров."""
     queryset = Genre.objects.all().order_by('name')
     serializer_class = GenreSerializer
+    permission_classes = IsAdminOrSuperuserPermission
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
     lookup_field = 'slug'
