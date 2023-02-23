e33fa6c65c178fb536a0676bce897f6725212792
Антон Богданович <bogianthony@gmail.com>
2023-02-24 02:48:02 +0300

    reviewer-answer-fix-v1.1

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -220,9 +220,9 @@ class ReviewSerializer(serializers.ModelSerializer):
         author = request.user
         title_id = self.context['view'].kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
-        if request.method == 'POST':
-            if Review.objects.filter(title=title, author=author).exists():
-                raise ValidationError()
+        if (request.method == 'POST' and
+            Review.objects.filter(title=title, author=author).exists()):
+            raise ValidationError()
         return data
 
     class Meta:
